a7dfd0865ae91ecae7e1d333ad7078d6e607d9eb
Liyanapk <liyana.kp8590@gmail.com>
2024-10-29 12:57:43 +0530

    node

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-var http = require('http');
-http.createServer(function(req, res) {
-    res.write("hi ")
-    res.end();
-}).listen(5000);
+// import http from "http";
+// import { URL } from "url";
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+const PORT = process.env.PORT || 6000
+
+const app = express()
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at  ${PORT}`)
+})
